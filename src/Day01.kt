53c1350feee559c3310c32effdc8a1d058cd4dc9
Ali-Akber Saifee <ali@indydevs.org>
2024-06-15 16:29:22 -0700

    Add Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
+import kotlin.text.Regex
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  val numbers =
+      mapOf(
+          "one" to "1",
+          "two" to "2",
+          "three" to "3",
+          "four" to "4",
+          "five" to "5",
+          "six" to "6",
+          "seven" to "7",
+          "eight" to "8",
+          "nine" to "9")
+  val expr = Regex("(?=(${numbers.keys.joinToString("|")}|${numbers.values.joinToString("|")}))")
+  fun part1(input: List<String>): Int {
+    return input.map { it.filter(Char::isDigit) }.sumOf { "${it.first()}${it.last()}".toInt() }
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part2(input: List<String>): Int {
+    return input
+        .map { expr.findAll(it).map { it.groupValues[1] }.map { numbers.getOrElse(it) { it } } }
+        .sumOf { "${it.first()}${it.last()}".toInt() }
+  }
+  check(part1(readInput("samples/01")) == 142)
+  check(part2(readInput("samples/01_2")) == 281)
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  val input = readInput("inputs/01")
+  part1(input).println()
+  part2(input).println()
 }
